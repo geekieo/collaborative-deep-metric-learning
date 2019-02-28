63bc60e30192c93efcf1c288c910c712e85883eb
geekieo <geekieo@hotmail.com>
2019-02-28 20:29:37 +0800

    update

--- imitation_data.py
+++ imitation_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import itertools
 import random 
 import sys
+from utils import exe_time
 
 
 # user
@@ -11,7 +12,7 @@ num_guid = 10000
 feature_size = 1500 
 
 
-def gen_unique_id_array(low, high, size,to_bytes=True):
+def gen_unique_id_array(low, high, size,to_bytes=False):
   """生成元素唯一的一维整数随机数组
   Args:
     low: int
@@ -36,7 +37,7 @@ def gen_unique_id_array(low, high, size,to_bytes=True):
   return ids
 
 
-def gen_features(num_feature, feature_size):
+def gen_features(num_feature, feature_size, decimals=8):
   """ 生成2D数组，元素范围在[-1,1]之间
   Args:
     num_feature: int
@@ -46,6 +47,7 @@ def gen_features(num_feature, feature_size):
   """
   shape = (num_feature, feature_size)
   features = np.random.random(shape)
+  features = np.around(features, decimals)
   return features
 
 
@@ -53,18 +55,21 @@ def combine_id_feature(ids, features):
   """ 组合 id_array 和 feature_array
   ids 的长度需等于 feature 的行数
   Arg:
-    ids: 1d array, []
+    ids: 1d array, [num_id]
     features: 2d array, [num_id, size_feature]
   Return:
     features: the first column is the id
   """
-  
-  return
+  ids = ids[:, np.newaxis] #1维转列向2维
+  ids = ids.astype(np.int)
+  features = np.hstack((ids,features)) #NOTE:如果 ids.dtype 是 np.bytes_ 速度会很慢
+  return features
 
 def run():
   uids = gen_unique_id_array(low=1, high=num_uid*2, size=num_uid)
   guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid)
   features = gen_features(num_feature=num_guid, feature_size=feature_size)
+  features = combine_id_feature(guids, features)
 
 if __name__ == "__main__":
     run()
--- tests/test_imitation_data.py
+++ tests/test_imitation_data.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.append("..")
+from utils import exe_time
 from imitation_data import num_uid, num_guid, feature_size
 from imitation_data import gen_unique_id_array
 from imitation_data import gen_features
+from imitation_data import combine_id_feature
 
 
 def test_gen_unique_id_array():
@@ -17,7 +19,11 @@ def test_gen_features(num_feature=num_guid, feature_size=feature_size):
   print(features.shape, sys.getsizeof(features), type(features[0]),features)
 
 
+def test_combine_id_feature():
+  guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, to_bytes=True)
+  features = gen_features(num_feature=num_guid, feature_size=feature_size)
+  features = exe_time(combine_id_feature)(guids, features)
+  # print(features)
+
 if __name__ == "__main__":
-  test_gen_unique_id_array()
-  test_gen_features()
-  
+  test_combine_id_feature()
