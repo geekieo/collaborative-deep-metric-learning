c899d53382982977f7edf73aafce0db14ff39367
geekieo <geekieo@hotmail.com>
2019-03-04 17:41:00 +0800

    模拟数据

--- imitation_data.py
+++ imitation_data.py
@@ -13,7 +13,7 @@ feature_size = 1500
 
 
 def gen_unique_id_array(low, high, size, dtype=None):
-  """生成元素唯一的一维整数随机数组
+  """生成元素唯一的一维整数随机数组,用于生成 uids 和 guids
   Args:
     low: int
     high: int
@@ -31,9 +31,9 @@ def gen_unique_id_array(low, high, size, dtype=None):
     raise("size is negative")
 
   random_list = list(itertools.product(range(low, high+1))) #唯一的二维整数顺序列表
-  ids = random.sample(random_list, size) #唯一的二维整数随机列表
-  ids = np.array(ids)                   #唯一的二维整数随机数组
-  ids = ids.flatten()                   #唯一的一维整数随机数组
+  ids = random.sample(random_list, size)  #唯一的二维整数随机列表
+  ids = np.array(ids)                     #唯一的二维整数随机数组
+  ids = ids.flatten()                     #唯一的一维整数随机数组
   if dtype:
     ids = ids.astype(dtype)
   return ids
@@ -53,51 +53,33 @@ def gen_features(num_feature, feature_size, decimals=8):
   return features
 
 
-def combine_id_feature(ids, features):
-  """ 组合 id_array 和 feature_array
-  ids 的长度需等于 feature 的行数
+def arrays_to_dict(array_1d,array_2d):
+  """ 将 1darray 和 array_2d 组合成一个字典
+  要求两个array行数相同，array_1d.shape[0] == array_2d.shape[0]
   Args:
-    ids: ndarray, [num_id]
-    features: ndarray, [num_id, size_feature]
+    array_1d: 1-D array-like 
+    array_2d: 2-D ndarray
   Return:
-    features: the first column is the id
+    dict. key 为 array_1d 的元素，value 为 array_2d 的元素
   """
-  ids = ids[:, np.newaxis] #1维转列向2维
-  # ids = ids.astype(np.int)
-  features = np.hstack((ids,features)) #NOTE:如果 ids.dtype 是 np.bytes_ 速度会很慢
-  return features
+  return dict(zip(array_1d, array_2d))
 
 
-def combine_feature_id(features, ids):
-  """ 组合 id_array 和 feature_array
-  ids 的长度需等于 feature 的行数
-  Args:
-    features: ndarray, [num_id, size_feature]
-    ids: ndarray, [num_id]
-  Return:
-    features: the last column is the id
-  """
-  ids = ids[:, np.newaxis] #1维转列向2维
-  # ids = ids.astype(np.int)
-  features = np.hstack((features,ids)) #NOTE:如果 ids.dtype 是 np.bytes_ 速度会很慢
-  return features
-
-
-def gen_single_watched_guids(guids, low, high):
-  """生成一条数量随机的 cowatch 数据
+def gen_watched_guids(guids, low, high):
+  """生成一条数量随机的 watched guids
   Args:
     guids: 1-D array-like, list of bytes.
     low: int.
     high: int.
   Return:
-    single_watched_guids: list of bytes. [size_single_watched]
+    watched_guids: list of bytes. [size_single_watched]
   """
-  size_single_watched = random.randint(low, high)
-  single_watched_guids = np.random.choice(guids, size_single_watched)
-  single_watched_guids = single_watched_guids.tolist()
-  return single_watched_guids
+  size_watched = random.randint(low, high)
+  watched_guids = np.random.choice(guids, size_watched)
+  watched_guids = watched_guids.tolist()
+  return watched_guids
 
-def gen_watched_guids(guids, num_cowatch, low=2, high=30):
+def gen_all_watched_guids(guids, num_cowatch, low=2, high=30):
   """ 在 guids 中随机挑选随机数目的 guid
   Args:
     guids: ndarray of bytes
@@ -105,20 +87,21 @@ def gen_watched_guids(guids, num_cowatch, low=2, high=30):
     low: int.Lowest (signed) integer to be drawn from the distribution 
     high: int, optional. If provided, one above the largest (signed) integer to be drawn from the distribution
   Return:
-    watched_guids: list of single_watched_guids, [num_cowatch].
+    all_watched_guids: list of watched_guids, [num_cowatch].
   """
-  watched_guids = []
+  all_watched_guids = []
   for _ in range(num_cowatch):
-    single_watched_guids = gen_single_watched_guids(guids, low, high)
-    watched_guids.append(single_watched_guids)
-  return watched_guids
-
+    watched_guids = gen_watched_guids(guids, low, high)
+    all_watched_guids.append(watched_guids)
+  return all_watched_guids
 
 
 def run():
   uids = gen_unique_id_array(low=1, high=num_uid*2, size=num_uid)
   guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid)
   features = gen_features(num_feature=num_guid, feature_size=feature_size)
+  print(features)
+
 
 if __name__ == "__main__":
   run()
