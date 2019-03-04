66ffe7a8b62547d84566cc2b4b4c31ad046d23df
wengjy1 <wengjy1@ifeng.com>
2019-03-04 17:41:00 +0800

    模拟数据

--- tests/test_imitation_data.py
+++ tests/test_imitation_data.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 import sys
 sys.path.append("..")
 import numpy as np
@@ -5,10 +6,9 @@ from utils import exe_time
 from imitation_data import num_uid, num_guid, feature_size
 from imitation_data import gen_unique_id_array
 from imitation_data import gen_features
-from imitation_data import combine_id_feature
-from imitation_data import combine_feature_id
-from imitation_data import gen_single_watched_guids
+from imitation_data import arrays_to_dict
 from imitation_data import gen_watched_guids
+from imitation_data import gen_all_watched_guids
 
 
 def test_gen_unique_id_array():
@@ -23,30 +23,27 @@ def test_gen_features(num_feature=num_guid, feature_size=feature_size):
   print(features.shape, sys.getsizeof(features), type(features[0]),features)
 
 
-def test_combine_id_feature():
-  guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, dtype=np.bytes_)
+def test_arrays_to_dict():
+  guids = gen_unique_id_array(low=1, high=num_guid, size=num_guid, dtype=np.bytes_)
   features = gen_features(num_feature=num_guid, feature_size=feature_size)
-  features = exe_time(combine_id_feature)(guids, features)
-  print(features)
-
-
-def test_combine_feature_id():
-  guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, dtype=np.bytes_)
-  features = gen_features(num_feature=num_guid, feature_size=feature_size)
-  features = exe_time(combine_feature_id)(features, guids)
-  print(features)
-
-
-def test_gen_single_watched_guids():
-  guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, dtype=np.bytes_)
-  single_watched_guids = gen_single_watched_guids(guids=guids, low=2, high=30)
-  print(single_watched_guids)
+  features = exe_time(arrays_to_dict)(guids, features)
+  for k,v in features.items():
+    print(k, ":\t", v)
+    break
 
 
 def test_gen_watched_guids():
   guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, dtype=np.bytes_)
-  watched_guids = gen_watched_guids(guids, num_cowatch=10, low=2, high=30)
+  watched_guids = gen_watched_guids(guids=guids, low=2, high=30)
+  print(watched_guids)
+
+
+def test_gen_all_watched_guids():
+  guids = gen_unique_id_array(low=1, high=num_guid*2, size=num_guid, dtype=np.bytes_)
+  watched_guids = gen_all_watched_guids(guids, num_cowatch=1000, low=2, high=30)
   print(watched_guids)
 
 if __name__ == "__main__":
-  test_gen_watched_guids()
+  # test_arrays_to_dict()
+  # test_gen_watched_guids()
+  test_gen_all_watched_guids()
