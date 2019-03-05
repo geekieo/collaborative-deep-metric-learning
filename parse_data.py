4046b3ca320c14f6542a3c4481a4baf0c97f2dec
wengjy1 <wengjy1@ifeng.com>
2019-03-05 14:18:57 +0800

    获取cowatch

--- parse_data.py
+++ parse_data.py
@@ -1,11 +1,6 @@
 # -*- coding:utf-8 -*-
 from functools import reduce
 
-def get_guids_index(guids):
-  """ guid 索引字典 
-  用于查找 guid 在 guids 中的索引
-  """
-  return {k: v for v, k in enumerate(guids)}
 
 def get_one_list_of_cowatch(watched_guids):
   """ 解析一组 watched_guids 的 co-watch
@@ -40,28 +35,29 @@ def yield_all_cowatch(all_watched_guids):
     yield cowatch
 
 
-def get_cowatch_graph(guids, all_watched_guids):
-  """统计所有观看历史，返回 cowatch 无向图，用稀疏矩阵表示，用于筛选 cowatch
-  NOTE: 如果显存不够，用 CPU 运行在内存上
-  Args:
-    guids: 
-    all_watched_guids: 2-D ndarray 
-  Retrun:
-    cowatch_graph: SparseTensor
+def get_guids_index(guids):
+  """ guid 索引字典 
+  用于查找 guid 在 guids 中的索引
   """
-  guids_size = len(guids)
-  dense_shape = [guids_size, guids_size]
-
-  guids_index = get_guids_index(guids)
-  # for watched_guids in all_watched_guids:
-    
-  # indices = 
-  # values = 
+  return {k: v for v, k in enumerate(guids)}
   
 
+def get_cowatch_graph(guids, all_watched_guids):
+  """统计所有观看历史，返回 cowatch 无向图，用于筛选 cowatch
+  TODO: 过滤 cowatch 少于固定阈值的 pair，过滤对角 pair
+  NOTE: 用 CPU 运行在内存上
+        可以用稀疏矩阵存储和操作
+  Args:
+    guids: list of int.
+    all_watched_guids: list of watched_guids(list of guids)
+  Retrun:
+    cowatch_graph
+  """
+  pass
 
 
-  
+def threshold_filter(cowatch_graph, threshold):
+  """ 使用 cowatch_graph 过滤低于 threshold 的 cowatch """
+  pass
 
 
-  
