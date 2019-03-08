5eae78a3d856a4d95e6d5dce36d9bd8dc7276928
wengjy1 <wengjy1@ifeng.com>
2019-03-08 17:46:55 +0800

    pipes

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -13,17 +13,19 @@ class test_TripletPipe():
     self.test_create_pipe()
     
   def test_create_pipe(self):
-    input_shape = (100, 3, 1500)
+    input_shape = (3, 2, 3)
     features = gen_features(num_feature=input_shape[0]*input_shape[1],
                             feature_size=input_shape[2])
     features = np.reshape(features,input_shape)
     pipe = TripletPipe()
-    input_tensor = pipe.create_pipe(features)
+    batch_size = 5
+    input_tensor = pipe.create_pipe(features, batch_size=batch_size, num_epochs=None)
     init_op = tf.global_variables_initializer()
     with tf.Session() as sess:
       sess.run(init_op)
       input_val = sess.run(input_tensor)
       print(input_val, input_val.shape)
+      assert input_val.shape == (batch_size, input_shape[1], input_shape[2])
 
 if __name__ == "__main__":
   test_TripletPipe()
