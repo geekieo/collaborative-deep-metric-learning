7d185712d6c14edb0d74a785c0e662e3f51d55c5
geekieo <geekieo@hotmail.com>
2019-03-11 10:54:36 +0800

    coding

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,1 +1,18 @@
-#def test_supervisor():
+# -*- coding:utf-8 -*-
+import sys
+sys.path.append("..")
+import numpy as np
+import tensorflow as tf
+import 
+
+import models
+import 
+from utils import find_class_by_name
+from train import Trainer
+
+
+train_dir="."
+model = find_class_by_name(FLAGS.model, [models])()
+
+class test_Trainer():
+  trainer = Trainer()
