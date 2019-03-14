60125ca6a9da296f97b4a98e31ad17ed41d05292
geekieo <geekieo@hotmail.com>
2019-03-14 11:24:14 +0800

    updata

--- inputs.py
+++ inputs.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 """Provides input pipe, which can get input data tensors for models."""
 import tensorflow as tf
+from tensorflow import logging
+
+logging.set_verbosity(logging.INFO)
 
 class BasePipe(object):
   """Inherit from this class when implementing new readers."""
@@ -12,7 +15,8 @@ class BasePipe(object):
 class TripletPipe(BasePipe):
   
   def create_pipe(self,
-                  data,
+                  data=None,
+                  filename=None,
                   batch_size=10,
                   num_epochs=None,
                   num_readers=1,
@@ -21,20 +25,30 @@ class TripletPipe(BasePipe):
     """Construct a memory data pipe.
     Args:
       data: ndarray of triplet([anchor feature, positive feature, negative feature])
-        all training data.
+            all training data.
+      filename: str. A path pattern to the data files. If 'data' and 'filename'
+                both are provided, then only data is used. 'data' and 'filename'
+                must have at least one.
       batch_size: How many examples to process at a time.
       num_epochs: How many passes to make over the training data. Set to 'None'
                   to run indefinitely.
       TODO num_readers: How many I/O threads to use.
     return:
-      A tensor containing a batch of features tensor(anchor, positive, negative)
+      An Iterator over the elements of this dataset. use Iterator.get_next() to
+      get the next batches of features tensor(anchor, positive, negative)
+    rasie:
     """
-    dataset = tf.data.Dataset.from_tensor_slices(data)
-    # Transformation: batch,shuffle,repeat
-    dataset = dataset.repeat(num_epochs)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.shuffle(buffer_size)
-    
-    # iterator
-    iterator = dataset.make_one_shot_iterator()
-    return iterator
+    if data:
+      dataset = tf.data.Dataset.from_tensor_slices(data)
+      # Transformation: batch,shuffle,repeat
+      dataset = dataset.repeat(num_epochs)
+      dataset = dataset.batch(batch_size)
+      dataset = dataset.shuffle(buffer_size)
+      # iterator
+      iterator = dataset.make_one_shot_iterator()
+      return iterator
+    elif filename:
+      pass
+    else:
+      raise ValueError('Input ERROR. Check argument "data" or "filename".')
+
--- losses.py
+++ losses.py
@@ -21,9 +21,10 @@ class HingeLoss(BaseLoss):
   def calculate_loss(self, triplets, margin=1):
     """ 
     Args:
-      triplets: 3-d tensor storing the embeddings. The dimensions are
-        [batch, triplet_channel, embedding]. The default labels of each
-        triplet are [anchor, positive, negative].
+      triplets: 3-d tensor storing the triplets. The dimensions are
+        [batch, triplet_channel, embedding]. The default triplet_channel is 3.
+        The default labels of each triplet are [anchor embedding, positive 
+        embedding, negative embedding].
       margin: int. The margin for triplet loss
     Retrun:
       A scalar loss tensor.
