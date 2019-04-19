8690c9ee228f8362ab0749459248b05fd674a01e
geekieo <geekieo@hotmail.com>
2019-04-19 11:46:30 +0800

    +1

--- online_data.py
+++ online_data.py
@@ -245,7 +245,7 @@ def write_cowatches(cowatches, save_dir='',split=4):
 
 def gen_training_data(watch_file, feature_file,threshold=3, base_save_dir='/.',split=4, unique=False):
   all_cowatch, features, encode_map, decode_map, graph = get_cowatches(watch_file, feature_file)
-  for threshold in range(1,threshold):
+  for threshold in range(1,threshold+1):
     for unique in [True, False]:
       save_dir = os.path.join(base_save_dir,'cdml_'+str(threshold)+('_unique' if unique else ''))
       if not os.path.exists(save_dir):
