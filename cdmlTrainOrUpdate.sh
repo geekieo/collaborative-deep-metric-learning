a37414f1792730bbcc935b0441b48af7095f3844
geekieo <geekieo@hotmail.com>
2019-05-16 15:59:26 +0800

    outline

--- cdmlTrainOrUpdate.sh
+++ cdmlTrainOrUpdate.sh
@@ -1,3 +1,4 @@
+# 切到 prod 用户
 #!/usr/bin/env bash
 # author @zhoukang
 source /etc/profile
@@ -10,8 +11,8 @@ training_dir=$project_dir/src
 serving_dir=$project_dir/src
 
 cur_date=`date +"%Y%m%d%H"`
-src_files=$project_dir/cdml_knn/$cur_date
-target_dir=/user/zhoukang/videoknn/cdml/$cur_date
+knn_files=$project_dir/knn_result/$cur_date        # 调整 knn_split 地址
+target_dir=/user/zhoukang/videoknn/cdml/$cur_date 
 # signal files
 signal_file=/user/zhoukang/videoknn/cdml/signal.txt
 update_signal_file=/user/zhoukang/video_clicks/cdml_update_signal.txt
@@ -22,9 +23,13 @@ if [ $? -eq 0 ];then
     ## training
     hadoop fs -rm -r $training_signal_file
     check_task "delete training signal file"
+    # todo: hadoop fs 
+    hadoop fs -getmerge /user/zhoukang/video_clicks/uid2records_cdml $project_dir/dataset/src_watch_history
+    hadoop fs -getmerge /user/zhoukang/tables/cdml_video_vec $project_dir/dataset/src_features
     # todo: train model
-
-    # todo:calc knn result use guid_knn.py (input encoded features output knn)
+    cd 
+    /data/python online_data.py --base_save_dir  "tests/" --feature_file "tests/visual_features.txt" --watch_file "tests/watched_guids.txt"
+    # 模型 -> serving_model
 
 else
     hadoop fs -test -e $update_signal_file
@@ -32,8 +37,12 @@ else
         ## updating
         hadoop fs -rm -r $update_signal_file
         check_task "delete update signal file"
+        # todo: hadoop fs 
+        hadoop fs -getmerge /user/zhoukang/tables/cdml_video_vec $project_dir/dataset/src_features
+        # serving_model predict 
+        # todo:calc knn result use guid_knn.py (input encoded features output knn)
         # todo: calc knn results of new vectors
-
+        # 先测 knn
     else
         echo "nothing to do, waiting..."
         exit 1
@@ -42,7 +51,7 @@ fi
 
 # put model to hdfs and send signal
 hadoop fs -mkdir -p $target_dir
-hadoop fs -put -f $src_files/knn_split* $target_dir
+hadoop fs -put -f $knn_files/knn_split* $target_dir
 
 # set finish signal
 hadoop fs -touchz $signal_file
