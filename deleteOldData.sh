ca9eeee6d050637bbce94c211a35fe00194f7789
翁敬喻 <wengjy1@ifeng.com>
2019-08-22 18:05:05 +0800

    延长日志清除时间

--- deleteOldData.sh
+++ deleteOldData.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 ### 
 # @Description: 
- # @Date: 2019-07-10 17:31:26
- # @Author: Weng Jingyu
- ###
-# 删除旧模型和旧结果
+# @Date: 2019-07-10 17:31:26
+# @Author: Weng Jingyu
+###
+# 删除旧结果
 source /etc/profile
 date=`date -d "7 days ago" +"%Y%m%d"`
 rm -rf /data/service/ai-algorithm-cdml/serving_dir/knn_result/$date??
+# 删除旧模型和日志
+date=`date -d "30 days ago" +"%Y%m%d"`
 rm -rf /data/service/ai-algorithm-cdml/serving_dir/models/$date??
 rm -rf /data/service/ai-algorithm-cdml/logs/update.$date.log
 rm -rf /data/service/ai-algorithm-cdml/logs/training.$date.log
